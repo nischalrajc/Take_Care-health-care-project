956fdc69b0a07d13690e42f1ad158a7149d4c857
nischalrajc <nischalraj22@gmail.com>
2024-03-07 15:10:32 +0530

    file deletion modified

--- backend/Services/Admin.js
+++ backend/Services/Admin.js
@@ -1,9 +1,31 @@
 import Doctors from "../Modal/Doctor.js"
 
-export const doctorNotauthorised = async()=>{
-    try{
+export const doctorNotauthorised = async () => {
+    try {
         const doctors = await Doctors.find({ authorised: false })
         return doctors
+    } catch (error) {
+        console.log("error", error)
+    }
+}
+
+export const getDoctor = async (id) => {
+    try {
+        const doctor = await Doctors.findById(id)
+        return doctor
+    } catch (error) {
+        console.log("error", error)
+    }
+}
+
+export const removeDoctor = async (id) => {
+    try{
+        const deleted_Doctor = await Doctors.findByIdAndDelete(id);
+        if(deleted_Doctor){
+            return true
+        }else{
+            return false
+        }
     }catch(error){
         console.log("error",error)
     }

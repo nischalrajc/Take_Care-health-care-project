5bd4761fc10ce0d1bf3b395fa0a2518b3aa238d2
nischalrajc <nischalraj22@gmail.com>
2024-04-08 11:20:58 +0530

    slots updated

--- frontend/src/Components/User/AppointmentScheduleModal.jsx
+++ frontend/src/Components/User/AppointmentScheduleModal.jsx
@@ -36,7 +36,6 @@ function AppointmentScheduleModal({ doctorId, isOpen, onclose }) {
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
-
     };
 
     const selectHandler = async (id) => {
@@ -73,7 +72,6 @@ function AppointmentScheduleModal({ doctorId, isOpen, onclose }) {
                                             <div className='ms-6'>
                                                 {availableSlots?.map((slot, index) => {
                                                     const formattedTime = new Date(slot.date).toLocaleTimeString();
-                                                    console.log(slot)
                                                     const Scheduled = slot.scheduled
 
                                                     return (
@@ -109,55 +107,13 @@ function AppointmentScheduleModal({ doctorId, isOpen, onclose }) {
                         Login to book an Appointment
                         <Link className='ms-2 underline text-blue-700' to='/login'>Click here</Link>
                         <div className='mt-2 text-center'>
-                            
-                            <Link  onClick={() => onclose(false)}>Cancel</Link>
+
+                            <Link onClick={() => onclose(false)}>Cancel</Link>
                         </div>
                     </div>
                 )
             }
         </div>
-        // <div className={`modal ${isOpen ? 'is-active' : ''}`} >
-        //     <div className="modal-content  rounded-lg overflow-hidden p-12 max-w-3xl mx-auto text-start">
-        //         <section className="modal-body">
-        //             <div className="flex">
-        //                 <Calendar minDate={new Date()} onChange={handleDateChange} />
-
-        //                 {
-        //                     availableSlots?.length > 0 ? (
-        //                         <div className='ms-6'>
-        //                             {availableSlots?.map((slot, index) => {
-        //                                 const formattedTime = new Date(slot.date).toLocaleTimeString();
-        //                                 console.log(slot)
-        //                                 const Scheduled = slot.scheduled
-
-        //                                 return (
-        //                                     <div
-        //                                     key={index}
-        //                                     onClick={Scheduled ? null : () => selectHandler(slot._id)}
-        //                                     className={`hover:cursor-pointer border border-gray-400 my-2 px-6 py-1 rounded-md text-center ${slotId ? 'bg-blue-500' : ''} ${Scheduled ? 'opacity-50 hover:cursor-not-allowed' : ''}`}
-        //                                 >
-        //                                     {formattedTime}
-        //                                     {Scheduled && <p className="already-booked-text">Already booked</p>}
-        //                                 </div>
-        //                                 );
-        //                             })}
-        //                         </div>
-        //                     ) : (
-        //                         <div className="ms-6 w-36 flex items-center ">
-        //                             <p>No available slots at this time choose another date</p>
-        //                         </div>
-        //                     )
-        //                 }
-
-
-        //             </div>
-        //         </section>
-        //         <footer className="modal-footer border-t pt-4 mt-4 flex flex-col sm:flex-row sm:justify-between">
-        //             <button onClick={submitHandler} className="button block w-full bg-[#E38569] hover:bg-[#e07757] text-white font-semibold py-2 px-4 rounded mb-2 sm:mb-0 sm:mr-2">Submit</button>
-        //             <button className="authbtn block w-full" onClick={() => onclose(false)}>Cancel</button>
-        //         </footer>
-        //     </div>
-        // </div>
     )
 }
 

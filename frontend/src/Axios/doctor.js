f11210334ee955d40e399c2143a3baaf81b7e703
nischalrajc <nischalraj22@gmail.com>
2024-04-18 11:21:36 +0530

    updated

--- frontend/src/Axios/doctor.js
+++ frontend/src/Axios/doctor.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const Axios =  axios.create({
-    baseURL: "http://localhost:5000/doctor",
+    baseURL: "https://takecareofficial.online/doctor",
     withCredentials:true
   });

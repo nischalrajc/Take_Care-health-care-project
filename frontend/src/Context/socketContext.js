6f788de838b99cb930f922f2a991abf9a4ceb72d
nischalrajc <nischalraj22@gmail.com>
2024-04-25 17:21:12 +0530

    main builded

--- frontend/src/Context/socketContext.js
+++ frontend/src/Context/socketContext.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://takecareofficial.online');
 
 const ContextProvider = ({ children }) => {
 
